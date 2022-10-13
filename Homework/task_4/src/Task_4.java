9e36d4ba3cef59867e0bedf0209b001c48f894d0
sofisofi01 <sofi.sofi.01@mail.ru>
2022-10-14 01:42:31 +0300

    new

---
+++ Homework/task_4/src/Task_4.java
@@ -0,0 +1,55 @@
+public class Task_4 implements Task_4_base {
+    @Override
+    public int[] subtask_1_arrays(int size, int a0, int d) {
+        // сгенерировать и вернуть массив размера size, содержащий элементы
+        // арифметической прогрессии с первым членом a0 и разностью d
+        return null;
+    }
+
+    @Override
+    public int[] subtask_2_arrays(int size) {
+        // сгенерировать и вернуть массив размера size, первые два элемента
+        // которого равны единице, а каждый следующий - сумме всех предыдущих
+        return null;
+    }
+
+    @Override
+    public int[] subtask_3_arrays(int size) {
+        // сгенерировать и вернуть массив размера size, содержащий первые
+        // size чисел последовательности фибоначчи.
+        // https://ru.wikipedia.org/wiki/Числа_Фибоначчи
+        return null;
+    }
+
+    @Override
+    public int subtask_4_arrays(int[] data) {
+        // Для данного массива вычислить максимальный элемент
+        return 0;
+    }
+
+    @Override
+    public int subtask_5_arrays(int[] data, int k) {
+        // Для данного массива вычислить максимальный элемент
+        // кратный k. Гарантируется, что как минумум один такой элемент
+        // в массиве есть
+        return 0;
+    }
+
+    @Override
+    public int[] subtask_6_arrays(int[] arr1, int[] arr2) {
+        // Даны два массива arr1, arr2.
+        // Произвести слияние данных массивов в один отсортированный
+        // по возрастанию массив.
+        return null;
+    }
+
+    @Override
+    public int[] subtask_7_arrays(int[] arr1, int[] arr2) {
+        // Даны два отсортированных по возрастанию массива arr1, arr2.
+        // Произвести слияние данных массивов в один также отсортированный
+        // по возрастанию массив.
+        // Используйте алгоритм, время работы которого будет пропорционально сумме
+        // размеров arr1 и arr2, а не их произведению
+        return null;
+    }
+}
