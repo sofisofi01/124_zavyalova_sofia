2b2120edc346bbc2648b5c33fa4cdcbc58505e6c
sofisofi01 <sofi.sofi.01@mail.ru>
2022-10-14 00:43:53 +0300

    done

--- Homework/task_2/src/Task_2.java
+++ Homework/task_2/src/Task_2.java
@@ -15,25 +15,39 @@ public class Task_2 implements Task_2_base {
 
     @Override
     public int subtask_2_while(int num) {
-        // Последовательность целых чисел p(n) определяется следующим образом:
-        // p(0) = 1
-        // p(k) = 2 * p(k - 1) + 6, k > 0
-        //Найти элемент последовательности с номером num
-        return 0;
+        int p = 1;
+        if (num == 0)
+            p=1;
+        else
+            while (num != 0){
+                p= 2*p + 6;
+                num--;
+            }
+        return p;
+
     }
 
     @Override
     public boolean subtask_3_while(int num, int base) {
-        // Проверить, является ли число num натуральной степенью числа base
-        return false;
+        int k=base;
+        while (k<num)
+            k=k*base;
+        return k == num;
     }
 
     @Override
     public int subtask_4_while(int start, int end) {
-        // Вычислить, за какое минимальное число операций
-        // вычесть 1
-        // поделить на 2
-        // число start можно превратить в end. Гарантируется, что start >= end >= 1
-        return 0;
+        int k=0;
+        while (start > end){
+            if ((start%2 != 0) || (start-end == 2)){
+                start--;
+                k++;
+            }
+            else {
+                start=start/2;
+                k++;
+            }
+        }
+        return k;
     }
 }
