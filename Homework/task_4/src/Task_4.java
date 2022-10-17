241a8c9d0e63e353b5829879e259b36d612ab6a2
sofisofi01 <sofi.sofi.01@mail.ru>
2022-10-17 23:44:20 +0300

    done

--- Homework/task_4/src/Task_4.java
+++ Homework/task_4/src/Task_4.java
@@ -1,55 +1,104 @@
 public class Task_4 implements Task_4_base {
     @Override
     public int[] subtask_1_arrays(int size, int a0, int d) {
-        // сгенерировать и вернуть массив размера size, содержащий элементы
-        // арифметической прогрессии с первым членом a0 и разностью d
-        return null;
+            int newArray[] = new int[size];
+            newArray[0]=a0;
+            for (int i = 1; i < size; i++){
+                newArray[i]=newArray[i-1]+d;
+            }
+            return newArray;
     }
 
     @Override
     public int[] subtask_2_arrays(int size) {
-        // сгенерировать и вернуть массив размера size, первые два элемента
-        // которого равны единице, а каждый следующий - сумме всех предыдущих
-        return null;
+        int newArray[] = new int[size];
+        int sum = 2;
+        for(int i = 0; i < size; i++)
+        {
+            if (i <= 1) {
+                newArray[i] = 1;
+            }
+            else {
+                newArray[i] = sum;
+                sum = sum + newArray[i];
+            }
+        }
+        return newArray;
     }
 
     @Override
     public int[] subtask_3_arrays(int size) {
-        // сгенерировать и вернуть массив размера size, содержащий первые
-        // size чисел последовательности фибоначчи.
-        // https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-        return null;
+        int newArray[] = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+            if (i == 0) {
+                newArray[i] = 0;
+            }
+            else {
+                if (i == 1){
+                    newArray[i] = 1;
+                }
+                else {
+                    newArray[i] = newArray[i - 1] + newArray[i - 2];
+                }
+            }
+        }
+        return newArray;
     }
 
     @Override
     public int subtask_4_arrays(int[] data) {
-        // Для данного массива вычислить максимальный элемент
-        return 0;
+        int max = data[0];
+        for(int i = 0; i < data.length; i++) {
+            if (data[i]>max){
+                max=data[i];
+            }
+        }
+        return max;
     }
 
     @Override
     public int subtask_5_arrays(int[] data, int k) {
-        // Для данного массива вычислить максимальный элемент
-        // кратный k. Гарантируется, что как минумум один такой элемент
-        // в массиве есть
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < data.length; i++) {
+            if ((data[i]>max) && (data[i] % k == 0)){
+                max=data[i];
+            }
+        }
+        return max;
+    }
+
+    public static void Sort(int[] a){
+        boolean sorted = false;
+        int temp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i+1]) {
+                    temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
     }
 
     @Override
     public int[] subtask_6_arrays(int[] arr1, int[] arr2) {
-        // Даны два массива arr1, arr2.
-        // Произвести слияние данных массивов в один отсортированный
-        // по возрастанию массив.
-        return null;
+        int[] arr3 = new int[arr1.length + arr2.length];
+        for(int i = 0; i < arr1.length; i++) {
+            arr3[i]=arr1[i];
+        }
+        for(int i = arr1.length; i < arr3.length; i++) {
+            arr3[i]=arr2[i-arr1.length];
+        }
+        Sort(arr3);
+        return arr3;
     }
 
     @Override
     public int[] subtask_7_arrays(int[] arr1, int[] arr2) {
-        // Даны два отсортированных по возрастанию массива arr1, arr2.
-        // Произвести слияние данных массивов в один также отсортированный
-        // по возрастанию массив.
-        // Используйте алгоритм, время работы которого будет пропорционально сумме
-        // размеров arr1 и arr2, а не их произведению
         return null;
     }
 }
