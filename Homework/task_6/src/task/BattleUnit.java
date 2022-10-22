ec792464447eb60e354fa3099bab286e9e61d6a0
sofisofi01 <sofi.sofi.01@mail.ru>
2022-10-22 23:14:03 +0300

    done

---
+++ Homework/task_6/src/task/BattleUnit.java
@@ -0,0 +1,68 @@
+package task;
+
+public class BattleUnit {
+    private int strength, armor, health, x, y, damage;
+    public BattleUnit(int strength, int armor, int health, int x, int y){
+        this.strength = strength;
+        this.armor = armor;
+        this.health = health;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getStrength(){
+        return strength;
+    }
+
+    public int getArmor(){
+        return armor;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void attacked(BattleUnit enemy){
+        if (armor>0) {
+            if (enemy.strength >= armor) {
+                damage = enemy.strength - armor;
+                health -= damage;
+            } else {
+                health = health;
+            }
+        }
+        else {
+            health -= enemy.strength;}
+    }
+
+
+    //перещение
+    public void moveUp(){
+        y--;
+    }
+
+    public void moveDown(){
+        y++;
+    }
+
+    public void moveLeft(){
+        x--;
+    }
+    public void moveRight(){
+        x++;
+    }
+
+    public  boolean isAlive(){
+        return health>0;
+    }
+
+
+}
