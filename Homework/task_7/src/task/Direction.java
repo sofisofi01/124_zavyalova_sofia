1dc25823449624dce8db8e472725983a1a72175a
sofisofi01 <sofi.sofi.01@mail.ru>
2022-11-25 22:20:55 +0300

    new

--- Homework/task_7/src/task/Direction.java
+++ Homework/task_7/src/task/Direction.java
@@ -1,18 +1,37 @@
 package task;
 
-public enum Direction {
-    private double x;
-    private double y;
-
-    public Vector2(double x, double y) {
-        this.x = x;
-        this.y = y;
+public enum Direction { North ("North"), South("South"), East("East"), West("West");
+    private String direct;
+    Direction(String direct){
+        this.direct = direct;
     }
 
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
+    public Vector2 move (Vector2 start, double distance) {
+        double startX = start.getX();
+        double startY = start.getY();
+
+        switch (direct) {
+            case "North" -> {
+                startY = startY - distance;
+                return new Vector2(startX, startY);
+            }
+
+            case "South" -> {
+                startY = startY + distance;
+                return new Vector2(startX, startY);
+            }
+
+            case "East" -> {
+                startX = startX + distance;
+                return new Vector2(startX, startY);
+            }
+
+            case "West" -> {
+                startX = startX - distance;
+                return new Vector2(startX, startY);
+            }
+
+        }
+        return start;
     }
 }
