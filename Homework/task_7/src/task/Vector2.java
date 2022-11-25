bd23ab19dc272d5dca406d93be5041418b608f8f
sofisofi01 <sofi.sofi.01@mail.ru>
2022-11-25 22:20:50 +0300

    new

---
+++ Homework/task_7/src/task/Vector2.java
@@ -0,0 +1,26 @@
+package task;
+
+public class Vector2 {
+
+    private double x;
+    private double y;
+
+    public Vector2 (double x, double y) {
+
+        this.y = y;
+        this.x = x;
+    }
+    
+    public double getY() {
+
+         return y;
+
+    }
+
+    public double getX() {
+
+        return x;
+
+    }
+
+}
