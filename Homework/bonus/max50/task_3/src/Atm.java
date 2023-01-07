312fa23e82e5b4782cb981e1f13c0eb90dcaf96b
Sofi Zav <sofi.sofi.01@mail.ru>
2023-01-07 14:31:57 +0300

    start task_3

---
+++ Homework/bonus/max50/task_3/src/Atm.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Atm {
+    private int quantity50;
+    private int quantity100;
+    private int quantity200;
+    private int quantity500;
+    private int quantity1000;
+    private int quantity2000;
+    private int quantity5000;
+}
