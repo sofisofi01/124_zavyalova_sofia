ec272c5906825a349d4ed824fa918f57cea61c9d
Sofi Zav <sofi.sofi.01@mail.ru>
2023-01-07 15:31:38 +0300

    task_6 done

---
+++ Homework/bonus/max50/task_6/src/SummCalculator.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+public class SummCalculator {
+    public int calculateSumm (HashMap<String, Integer> quantity, HashMap<String, Integer> price) {
+        int sum = 0;
+
+        for (Map.Entry entry : quantity.entrySet()) {
+            if (price.containsKey(entry.getKey())) {
+                sum = sum + price.get(entry.getKey()) * price.get(entry.getKey());
+            }
+        }
+        return sum;
+    }
+}
